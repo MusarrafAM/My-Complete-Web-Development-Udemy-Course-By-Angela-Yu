0b232d9da4fc6817987cf942bc1692cec881c1f0
Musarraf <muhammedmusarraf@gmail.com>
2023-06-24 08:38:08 +0530

    Add DOM Lessons

---
+++ 16 DOM (Document Object Model)/DOM Challenge Starting Files/index.js
@@ -0,0 +1,8 @@
+
+document.getElementsByTagName("li")[2].style.color = "orange";
+document.querySelector(".btn").style.color = "purple";
+document.querySelector("li a").style.color ="green";
+
+document.querySelector(".btn").style.backgroundColor = "yellow";
+
+document.querySelector("h1").classList.add("huge");
