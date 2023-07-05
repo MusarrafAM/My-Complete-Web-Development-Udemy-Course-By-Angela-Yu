f3a0cc37d99e540df6f28e502430ef51f915e2a1
Musarraf <muhammedmusarraf@gmail.com>
2023-07-06 00:13:15 +0530

    Blog Capstone project 3 and Arrow Funcion

---
+++ 28. Capstone project - 3 Blog/Arrow function and forEach/app.js.js
@@ -0,0 +1,32 @@
+
+// !Normal Fucntion
+// let greet = function(name){
+//     console.log("Welcome "+ name);
+// }
+// greet("Musarraf")
+
+
+
+// !Arrow Fucntion
+// let greet = (name) =>{
+//     console.log("Welcome "+ name);
+// }
+// greet("Musarraf")
+
+// !Function with only 1 statement, we don't need to add curly brackets.
+// let add = (n1,n2) => n1 +n2;
+
+// console.log(add(2,8));
+
+// !ForEach loop
+let fruits = ["Apple", "Orange", "Grapes", "Mangos"]
+
+// fruits.forEach(friut => {
+//     console.log(friut);
+// });
+
+
+fruits.forEach(function (fruit) {
+    console.log(fruit);
+  })
+
