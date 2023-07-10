7fc50785cd4983f9f61915479cc310f5cf1d7363
Musarraf <muhammedmusarraf@gmail.com>
2023-07-10 12:53:45 +0530

    add mongodb my user to .env files

--- 35 - Blog website Upgrade With Mongodb(Atlas)/app.js
+++ 35 - Blog website Upgrade With Mongodb(Atlas)/app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const ejs = require("ejs");
 const _ = require('lodash');   // To use lowerCase()
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -21,7 +22,7 @@ main().catch(err => console.log(err));
 
 
 async function main() {
-  await mongoose.connect('mongodb+srv://musa:5EX1IEKkwojM7RAO@cluster0.h07q12i.mongodb.net/blogsDB');
+  await mongoose.connect(process.env.MONGOMYATLAS);
 
 
   const blogSchema = new mongoose.Schema({     //Creating Blog Schema 
@@ -99,10 +100,6 @@ async function main() {
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
-
-
-
-
 
 
 
